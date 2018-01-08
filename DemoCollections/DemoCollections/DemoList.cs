ccd5353d099f7a5718289b1ab51aae739356ffe7
akashvema1992 <av8800919186@gmail.com>
2018-01-08 22:50:14 +0530

    first commit

---
+++ DemoCollections/DemoCollections/DemoList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoCollections {
+    class DemoList {
+        static void Main(string[] args) {
+            IList<String> list = new List<String>();
+            for (int i=0; i<10; i++)
+                list.Add(i.ToString());
+            foreach (var i in list)
+                Console.Write(i + " ");
+            list.Add("10");
+
+            Console.Write("\n");
+            foreach (var i in list)
+                Console.Write(i + " ");
+
+            Console.Write("\n");
+            IEnumerator<String> it = list.GetEnumerator();
+            while (it.MoveNext()) {
+                Console.Write(it.Current + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
