4b32773bc6e18f52b5ef19579eedd6a190e3708a
akashvema1992 <av8800919186@gmail.com>
2018-01-10 00:29:02 +0530

    commit

--- DemoWebAPIs/ProductsApp/Controllers/ProductsController.cs
+++ DemoWebAPIs/ProductsApp/Controllers/ProductsController.cs
@@ -1,27 +1,31 @@
-﻿using System;
+﻿using ProductsApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
-using ProductsApp.Models;
-using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Data;
 
-namespace ProductsApp.Controllers
-{
+namespace ProductsApp.Controllers {
     public class ProductsController : ApiController
     {
-        Product[] products = new Product[] {
-            new Product{Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1},
-            new Product{Id = 2, Name = "Yo-Yo", Category = "Toys", Price = 8.20M},
-            new Product{Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M}
-        };
-
+        //Product[] products = new Product[] {
+        //    new Product{Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1},
+        //    new Product{Id = 2, Name = "Yo-Yo", Category = "Toys", Price = 8.20M},
+        //    new Product{Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M}
+        //};
+        private ProductsDbContext db = new ProductsDbContext();
         public IEnumerable<Product> GetAllProducts() {
-            return products;
+            var products = from P in db.Products
+                           where P.Id > 0
+                           select P;
+            return (IEnumerable<Product>) products.ToList();
         }
 
         public IHttpActionResult GetProduct(int id) {
+            var products = from P in db.Products
+                           where P.Id > 0
+                           select P;
             var product = products.FirstOrDefault((p) => p.Id == id);
             if (product == null)
                 return NotFound();
@@ -44,14 +48,21 @@ namespace ProductsApp.Controllers
 
         [HttpPost]
         public IHttpActionResult PostData(Product product) {
-            ProductsDbContext db = new ProductsDbContext();
-            db.Products.Add(new Product { Id = product.Id,
-                Name = product.Name,
-                Category = product.Category,
-                Price = product.Price }
-            );
-            db.SaveChanges();
-            return this.Json(new { msg = "success" });
+            try {
+                ProductsDbContext db = new ProductsDbContext();
+                db.Products.Add(product);
+                db.SaveChanges();
+                return this.Json(new { msg = "success" });
+            } catch (SqlException ex) {
+                Console.Write(ex);
+            }
+            return this.Json(new { msg = "fail" });
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteData(int id) {
+
+            return this.Json(new { msg = "Deleted!" });
         }
     }
 }
